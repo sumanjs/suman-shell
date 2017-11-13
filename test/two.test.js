5dfac909268694a22bcac21ee15b5d1d657b28d9
Olegzandr VD <alex@oresoftware.com>
2017-11-12 23:41:49 -0800

    pdev:set

--- test/two.test.js
+++ test/two.test.js
@@ -1,6 +1,5 @@
 const suman = require('suman');
 const Test = suman.init(module);
-
 const Promise = require('bluebird');
 
 Test.create('firm', function (it) {
@@ -12,7 +11,5 @@ Test.create('firm', function (it) {
     });
 
   });
-
-  it('fails', suman.autoFail);
 
 });
